72df54a3c4d36439fea23ec2d40cfe9cba670bbc
dekel ido <dookido@gmail.com>
2022-09-15 01:06:26 +0300

    sass home-page

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
+export function AppHeader () {
+  return (
+    <header className='app-header'>
+      <section className='logo-container'>
+        <Link to='/'>
+          <img
+            className='logo'
+            src={require(`../assets/img/logo.gif`)}
+            alt=''
+          />
 
+          <span>Trello</span>
+        </Link>
 
-export function AppHeader() {
-
-    return (
-        <header className="app-header">
-       
-            <h1>DYERELLOR</h1>
-        </header>
-    )
+      </section>
+        <div className='avatar-img'></div>
+    </header>
+  )
 }
-
