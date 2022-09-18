0676499aebfb7f9f5a6581ba137889267aeef4f1
dekel ido <dookido@gmail.com>
2022-09-18 11:58:46 +0300

    demo

--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -66,8 +66,8 @@ export function GroupList() {
               xmlns='http://www.w3.org/2000/svg'
             >
               <path
-                fill='none'
-                stroke='#000'
+                fill='white'
+                stroke='#fff'
                 strokeWidth='2'
                 d='M12,22 L12,2 M2,12 L22,12'
               ></path>
