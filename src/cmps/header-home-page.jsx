9d73faa4ad30fa942d221b6ccd734723d787976d
eldadYikne <nmknv99@gmail.com>
2022-09-23 00:08:21 +0300

    commit ey

---
+++ src/cmps/header-home-page.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Logo from '../assets/img/logo.gif'
+import { ReactComponent as LogoSvg } from '../assets/img/skyllo-logo.svg';
+
+
+
+export const HomePageHeader = () => {
+    const [imgSrc, setImgSrc] = useState(Logo)
+
+    return <section className="home-page-header">
+        <section className='logo-container-home-page'>
+            <Link to='/'>
+                <LogoSvg  stroke="#0052cc" fill=" #0052cc" />
+                {/* <img
+                    className='logo-home-page'
+                    src={Logo}
+                    alt=''
+                /> */}
+
+                <span className='h1-logo-home-page'>Skyllo</span>
+            </Link>
+        </section>
+        <section className="home-page-login-signup">
+
+            <Link className="home-page-login" to='/login'>
+                Login
+            </Link>
+
+
+            <Link className="home-page-signup" to='/login'>
+                Get Skyllo for free
+            </Link>
+
+
+        </section>
+
+    </section>
+}
