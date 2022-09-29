61a89a99374faf06eeaa09354619582eec27750c
Yaara Yehuda <yaarasas2@gmail.com>
2022-09-29 11:45:28 +0300

    dc

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,14 +1,11 @@
 import io from 'socket.io-client'
 import { userService } from './user.new.service'
 
-export const SOCKET_EVENT_ADD_MSG = 'chat-add-msg'
-export const SOCKET_EMIT_SEND_MSG = 'chat-send-msg'
-export const SOCKET_EMIT_SET_TOPIC = 'chat-set-topic'
-export const SOCKET_EMIT_USER_WATCH = 'user-watch'
+
 export const SOCKET_EVENT_USER_UPDATED = 'user-updated'
 export const SOCKET_EVENT_BOARD_ADDED = 'board-added'
 export const SOCKET_EVENT_BOARD_UPDATED = 'board-updated'
-export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you'
+
 
 const SOCKET_EMIT_LOGIN = 'set-user-socket'
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
